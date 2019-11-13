1c2faaae93417ef13e6720602b784241db6b06a7
Yahoo-back <2928501731@qq.com>
2019-11-13 16:31:02 +0800

    angular cart demo

---
+++ src/app/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    name: 'Iphone',
+    price: '1000',
+    description: 'large screen'
+  },
+  {
+    name: 'honor',
+    price: '1001',
+    description: 'good'
+  },
+  {
+    name: 'huawei',
+    price: '1002',
+    description: 'very good'
+  },
+  {
+    name: 'oppo',
+    price: '1003',
+    description: ''
+  }
+];
